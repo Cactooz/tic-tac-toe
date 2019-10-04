fe9fdd869a770d7bfc71641ce7812724ced00676
hugobacher <huba02001@utb.vaxjo.se>
2019-10-04 09:46:16 +0200

    Metod för gameboard print

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -5,7 +5,7 @@ namespace tic_tac_toe
     class Program
     {
         static void Main(string[] args)
-        {
+{
             Console.WriteLine("Tic Tac Toe");
             char[,] gameboard = new char[,] {
                 {'█', '█', '█' },
@@ -17,9 +17,18 @@ namespace tic_tac_toe
             gameboard[1, 1] = 'O';
             gameboard[1, 2] = 'X';
 
-            for (int x = 0; x < gameboard.GetLength(0); x++) {
-                for (int y = 0; y < gameboard.GetLength(1); y++) {
-                    Console.Write(gameboard[x, y]);
+
+            Print(gameboard);
+
+        }
+        static void Print(char[,] board)
+        {
+            //Skriver ut brädet
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    Console.Write($"{board[x, y]} ");
                 }
                 Console.WriteLine();
             }
