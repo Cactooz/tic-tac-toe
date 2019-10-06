3bd467d22f3bd5a925a0c83e357b6ae303e41c7d
TheCactusMonkey <TheCactusMonkey@users.noreply.github.com>
2019-10-06 20:04:51 +0200

    Nytt bräde och array

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -7,32 +7,25 @@ namespace tic_tac_toe
         static void Main(string[] args)
 {
             Console.WriteLine("Tic Tac Toe");
-            char[,] gameboard = new char[,] {
-                {'█', '█', '█' },
-                {'█', '█', '█' },
-                {'█', '█', '█' }
-            };
+            char[] gameboard = { '█', '█', '█', '█', '█', '█', '█', '█', '█' };
 
-            gameboard[0, 1] = 'X';
-            gameboard[1, 1] = 'O';
-            gameboard[1, 2] = 'X';
+            gameboard[0] = 'X';
+            gameboard[1] = 'O';
+            gameboard[2] = 'X';
 
             Print(gameboard);
 
         }
-        static void Print(char[,] board)
+        static void Print(char[] board)
         {
             //Rensa konsollen
             Console.Clear();
             //Skriver ut brädet
-            for (int x = 0; x < board.GetLength(0); x++)
-            {
-                for (int y = 0; y < board.GetLength(1); y++)
-                {
-                    Console.Write($"{board[x, y]} ");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");
+            Console.WriteLine("——————————");
+            Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
+            Console.WriteLine("——————————");
+            Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
         }
     }
 }
