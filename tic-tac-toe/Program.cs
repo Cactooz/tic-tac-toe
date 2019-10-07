d8743a9a4c7990a662ce2f8a74f948433ea52265
Hugo Bachér <54435874+hugobacher@users.noreply.github.com>
2019-10-07 10:38:59 +0200

    Nya board variabler, metoder för de olika delarna & tutorial

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -1,31 +1,82 @@
-﻿using System;
+using System;
 
 namespace tic_tac_toe
 {
     class Program
     {
         static void Main(string[] args)
-{
-            Console.WriteLine("Tic Tac Toe");
-            char[] gameboard = { '█', '█', '█', '█', '█', '█', '█', '█', '█' };
+        {
+            char[] mainBoard = { '█', '█', '█', '█', '█', '█', '█', '█', '█' };
 
-            gameboard[0] = 'X';
-            gameboard[1] = 'O';
-            gameboard[2] = 'X';
+            mainBoard[0] = 'X';
+            mainBoard[1] = 'O';
+            mainBoard[2] = 'X';
 
-            Print(gameboard);
-
+            Menu(mainBoard);
         }
-        static void Print(char[] board)
+
+        //Main menu
+        static void Menu(char[] menuBoard)
+        {
+            Console.Clear();
+            Console.WriteLine("Tic Tac Toe\n");
+            Console.WriteLine("To start playing type PLAY\nTo see the tutorial type TUTORIAL");
+            string startStatus = "";
+            while (startStatus != "play" || startStatus != "tutorial") {
+              string start = Console.ReadLine();
+              startStatus = start.ToLower();
+              if (startStatus == "play")
+                  Game(menuBoard);
+              else if (startStatus == "tutorial")
+                  Tutorial(menuBoard);
+            }
+        }
+
+        //Runs the game
+        static void Game(char[] gameBoard)
+        {
+            char player = 'X';
+            int turn = 0;
+            Print(gameBoard);
+            Console.WriteLine($"Turn: {turn+1}");
+            Console.WriteLine($"It's {player}'s turn");
+        }
+
+        //Tutorial
+        static void Tutorial(char[] tutorialBoard)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+            Console.WriteLine("This is a two player game where you plays X's or O's.");
+            Console.WriteLine("This is the game board:\n");
+            Console.WriteLine("1 | 2 | 3");
+            Console.WriteLine("—————————");
+            Console.WriteLine("4 | 5 | 6");
+            Console.WriteLine("—————————");
+            Console.WriteLine("7 | 8 | 9\n");
+            Console.WriteLine("When playing type the number where you want to place your X or O.\n");
+            Console.WriteLine("Done reading? Type DONE to get back to the main menu!");
+            string done = "";
+            while (done != "done") {
+              string answer = Console.ReadLine();
+              done = answer.ToLower();
+              if (done == "done")
+                  Menu(tutorialBoard);
+            }
+        }
+
+        //Prints the gameboard
+        static void Print(char[] printBoard)
         {
             //Rensa konsollen
             Console.Clear();
             //Skriver ut brädet
-            Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");
-            Console.WriteLine("——————————");
-            Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
-            Console.WriteLine("——————————");
-            Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
+            Console.WriteLine("Tic Tac Toe\n");
+            Console.WriteLine($"{printBoard[0]} | {printBoard[1]} | {printBoard[2]}");
+            Console.WriteLine("—————————");
+            Console.WriteLine($"{printBoard[3]} | {printBoard[4]} | {printBoard[5]}");
+            Console.WriteLine("—————————");
+            Console.WriteLine($"{printBoard[6]} | {printBoard[7]} | {printBoard[8]}\n");
         }
     }
 }
